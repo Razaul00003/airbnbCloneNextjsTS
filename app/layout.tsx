5d12df13d6cbf18b2631c85787bc9034a710710f
Razaul00003 <mdrazaulkarim00003@gmail.com>
2023-05-06 10:43:32 +0600

    added modal structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <Modal isOpen />
           <Navbar />
         </ClientOnly>
         {children}

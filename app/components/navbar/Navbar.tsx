14e3a4be5b872165b15cd06fa3861918940c8d0a
Razaul00003 <mdrazaulkarim00003@gmail.com>
2023-05-09 23:30:38 +0600

    added autoget current user

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -2,9 +2,14 @@
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
+import { User } from "@prisma/client";
 import UserMenu from "./UserMenu";
 
-const Navbar = () => {
+interface NavbarProps {
+  currentUser?: User | null;
+}
+
+const Navbar: React.FC<NavbarProps> = () => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div

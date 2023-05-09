e0e99499f67b773e6725726b4899669e892e58db
Razaul00003 <mdrazaulkarim00003@gmail.com>
2023-05-09 23:15:21 +0600

    'added login functionality'

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
         <ClientOnly>
           <ToasterProvider />
           <LoginModal />
+          <RegisterModal />
           <Navbar />
         </ClientOnly>
         {children}

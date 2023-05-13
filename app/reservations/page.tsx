5b8020d4a2dc349b3d6146087dd6d98d1076b1e3
Razaul00003 <mdrazaulkarim00003@gmail.com>
2023-05-13 11:27:34 +0600

    fixed: import error on reservation page

--- app/reservations/page.tsx
+++ app/reservations/page.tsx
@@ -4,8 +4,7 @@ import ClientOnly from "@/app/components/ClientOnly";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import getReservations from "@/app/actions/getReservations";
 
-import TripsClient from "./ReservationsClient";
-
+import TripsClient from "./reservationsClient";
 const ReservationsPage = async () => {
   const currentUser = await getCurrentUser();
 
